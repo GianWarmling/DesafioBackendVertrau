fc8200911b829b72a89241652e58a78d63a305d3
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2026-05-25 22:17:42 -0300

    Iniciada controller Usuario, Get e Get/Id criados

---
+++ DesafioBackendVertrau/DesafioBackendVertrau/Controllers/UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using DesafioBackendVertrau.Data;
+using DesafioBackendVertrau.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DesafioBackendVertrau.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> Get()
+        {
+            var usuarios = await _context.Usuarios.ToListAsync();
+            return Ok(usuarios);
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetById(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+            return Ok(usuario);
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

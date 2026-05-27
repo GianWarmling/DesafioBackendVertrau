df6a275db306b704358be64485abc2069b75e3f3
Gian Warmling <130003869+GianWarmling@users.noreply.github.com>
2026-05-26 22:31:20 -0300

    POST, PUT e DELETE criados

--- DesafioBackendVertrau/DesafioBackendVertrau/Controllers/UsuarioController.cs
+++ DesafioBackendVertrau/DesafioBackendVertrau/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using DesafioBackendVertrau.Data;
 using DesafioBackendVertrau.Models;
+using DesafioBackendVertrau.Models.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -39,20 +40,82 @@ namespace DesafioBackendVertrau.Controllers
 
         // POST api/<UsuarioController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult> Post([FromBody] Usuario usuario)
         {
+            var emailExiste = await _context.Usuarios.AnyAsync(u => u.Email == usuario.Email);
+
+            if (emailExiste)
+            {
+                return BadRequest("Email já cadastrado!");
+            }
+
+            if (usuario.DataNascimento > DateTime.Now)
+            {
+                return BadRequest("Data de nascimento não pode ser futura!");
+            }
+
+            if(!Enum.IsDefined(typeof(Genero), usuario.Genero))
+            {
+                return BadRequest("Gênero inválido!");
+            }
+
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+            return Ok(usuario);
         }
 
         // PUT api/<UsuarioController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] Usuario usuario)
         {
+            var usuarioBanco = await _context.Usuarios.FindAsync(id);
+
+            if (usuarioBanco == null) 
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+
+            var emailExiste = await _context.Usuarios.AnyAsync(u => u.Email == usuario.Email && u.Id != id);
+
+            if (emailExiste)
+            {
+                return BadRequest("Email já cadastrado!");
+            }
+
+            if (usuario.DataNascimento > DateTime.Now)
+            {
+                return BadRequest("Data de nascimento não pode ser futura!");
+            }
+
+            if (!Enum.IsDefined(typeof(Genero), usuario.Genero))
+            {
+                return BadRequest("Gênero inválido!");
+            }
+
+            usuarioBanco.Nome = usuario.Nome;
+            usuarioBanco.Sobrenome = usuario.Sobrenome;
+            usuarioBanco.Email = usuario.Email;
+            usuarioBanco.Genero = usuario.Genero;
+            usuarioBanco.DataNascimento = usuario.DataNascimento;
+
+            await _context.SaveChangesAsync();
+            return Ok("Usuário atualizado com sucesso!");
         }
 
         // DELETE api/<UsuarioController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return Ok("Usuário removido com sucesso!");
         }
     }
 }
